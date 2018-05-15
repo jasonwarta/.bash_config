d7a3876b2b83e94276879c078b5a6569447400b9
Jason Warta <jasonwarta@gmail.com>
2018-05-15 14:25:46 -0800

    tweaked nstall script to support update-style install

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 TARGET="$HOME/.bash_config"
+DOWNLOAD_LOCATION="$HOME"
 RELOAD_PROMPT=""
 
-wget https://github.com/jasonwarta/.bash_config/archive/master.zip -O ~/Downloads/bash_config.zip &&
-unzip $HOME/Downloads/bash_config.zip -d $HOME/Downloads/bash_config &&
-mv $HOME/Downloads/bash_config/.bash_config-master $TARGET &&
-rm -r $HOME/Downloads/bash_config $HOME/Downloads/bash_config.zip &&
+wget https://github.com/jasonwarta/.bash_config/archive/master.zip -qO $DOWNLOAD_LOCATION/bash_config.zip &&
+unzip -q $DOWNLOAD_LOCATION/bash_config.zip -d $DOWNLOAD_LOCATION/bash_config &&
+if [ -d $TARGET ]; then
+    mv $DOWNLOAD_LOCATION/bash_config/.bash_config-master/* $TARGET
+else 
+    mv $DOWNLOAD_LOCATION/bash_config/.bash_config-master $TARGET
+fi &&
+rm -r $DOWNLOAD_LOCATION/bash_config $DOWNLOAD_LOCATION/bash_config.zip &&
 mkdir -p $TARGET/local_settings &&
 touch $TARGET/local_settings/{path.sh,aliases.sh,functions.sh} && 
 if [ -r $HOME/.bashrc ]; then
